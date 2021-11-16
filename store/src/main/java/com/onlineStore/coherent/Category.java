285c042561198201214dee57da261d0babe7af16
AndreiSkBaron <baron_11_2012@mail.ru>
2021-11-16 15:02:15 +0200

    Initial commit

---
+++ store/src/main/java/com/onlineStore/coherent/Category.java
@@ -0,0 +1,18 @@
+package com.onlineStore.coherent;
+
+public class Category {
+    String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category '" + name + '\'';
+    }
+}
