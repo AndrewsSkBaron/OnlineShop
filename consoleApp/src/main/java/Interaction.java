f1709521a1509309adfbad41e4a495a0ae23f16f
AndrewsSkBaron <baron_11_2012@mail.ru>
2021-11-30 12:56:04 +0200

    Commit init #5 collections

--- consoleApp/src/main/java/Interaction.java
+++ consoleApp/src/main/java/Interaction.java
@@ -17,55 +17,72 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Interaction {
-        private RandomStorePopulator random = new RandomStorePopulator();
-        private final Store store = random.getRandomStore();
-        private final Parser parser = new Parser();
-        private final Root root = parser.parse();
-        private final Sort sort = root.getSort();
 
-        public List<Product> collectAllProductsInAnArray() {
-            List<Product> productsAll = new ArrayList<>();
-            for (Category category : store.getCategories()) {
-                productsAll.addAll(category.getListOfProducts());
-            }
-            return productsAll;
+    private RandomStorePopulator random = new RandomStorePopulator();
+    private final Store store = random.getRandomStore();
+    private final Parser parser = new Parser();
+    private final Root root = parser.parse();
+    private final Sort sort = root.getSort();
+
+    public List<Product> collectAllProductsInAnArray() {
+        List<Product> productsAll = new ArrayList<>();
+        for (Category category : store.getCategories()) {
+            productsAll.addAll(category.getListOfProducts());
         }
-
-        private List<Product> getSortRandomProduct(List<Product> productsAll,Sort sort) {
-            List<Product> sortProductsAll = new ArrayList<>(productsAll);
-
-            sortProductsAll.sort((o1, o2) -> {
-                int name = o1.getName().compareToIgnoreCase(o2.getName());
-                int price = Double.compare(o1.getPrice(), o2.getPrice());
-                switch (sort.getName()) {
-                    case "asc":
-                        if (name != 0) return name;
-                        break;
-                    case "desc":
-                        if (name != 0) return -name;
-                        break;
-                    default:
-                        return 0;
-                }
-                switch (sort.getPrice()) {
-                    case "asc":
-                        if (price != 0) return price;
-                        break;
-                    case "desc":
-                        if (price != 0) return -price;
-                        break;
-                    default:
-                        return 0;
-                }
-                return 0;
-            });
-            return sortProductsAll;
-        }
-    public void printOutAllSortedProducts() {
-        List<Product> sortedProducts = getSortRandomProduct(collectAllProductsInAnArray(), sort);
-        sortedProducts.forEach(System.out::println);
+        return productsAll;
     }
-   public void getBestOfProducts() {
+
+    private List<Product> getSortRandomProduct(List<Product> productsAll, Sort sort) {
+        List<Product> sortProductsAll = new ArrayList<>(productsAll);
+
+        sortProductsAll.sort((o1, o2) -> {
+            int nameComp = o1.getName().compareToIgnoreCase(o2.getName());
+            int priceComp = Double.compare(o1.getPrice(), o2.getPrice());
+            int rateComp = Double.compare(o1.getRate(), o2.getRate());
+            switch (sort.getName()) {
+                case "asc":
+                    if (nameComp != 0) return nameComp;
+                    break;
+                case "desc":
+                    if (nameComp != 0) return -nameComp;
+                    break;
+                case "no":
+                    break;
+            }
+            switch (sort.getPrice()) {
+                case "asc":
+                    if (priceComp != 0) return priceComp;
+                    break;
+                case "desc":
+                    if (priceComp != 0) return -priceComp;
+                    break;
+                case "no":
+                    break;
+            }
+            switch (sort.getRate()) {
+                case "asc":
+                    if (rateComp != 0) return rateComp;
+                case "desc":
+                    if (rateComp != 0) return -rateComp;
+                default:
+                    return 0;
+            }
+        });
+        return sortProductsAll;
+    }
+
+    public void printOutAllSortedProducts() {
+        List<Product> sortProductsAll = getSortRandomProduct(collectAllProductsInAnArray(), sort);
+        System.out.println(sortProductsAll);
+    }
+
+    public void showSortByNamePrice() {
+        List<Product> sortProductsAll = getSortRandomProduct(collectAllProductsInAnArray(), sort);
+        Collections.sort(sortProductsAll, new SortByName().thenComparing(new SortByPrice()));
+        sortProductsAll.forEach(System.out::println);
+    }
+
+    public void showBestOfProdByRate() {
         List<Product> sortProductsAll = getSortRandomProduct(collectAllProductsInAnArray(), sort);
         Collections.sort(sortProductsAll, new SortByRate());
         for (int i = 0; i < 5; i++) {
@@ -74,39 +91,41 @@ public class Interaction {
 
     }
 
-        public void scannerUserInteraction() {
-            Scanner scanner = new Scanner(System.in);
-            printOutInfoOfHelp();
-            quit:
-            while (true) {
-                String option = scanner.nextLine();
-                switch (option) {
-                    case "sort":
-                        printOutAllSortedProducts();
-                        System.out.println();
-                        break;
-                    case "top":
-                        getBestOfProducts();
-                        System.out.println();
-                        break;
-                    case "quit":
-                        System.out.println();
-                        System.out.println("Bye Bye");
-                        break quit;
-                    default:
-                        printOutInfoOfHelp();
-                }
+    public void scannerUserInteraction() {
+        Scanner scanner = new Scanner(System.in);
+        printOutAllSortedProducts();
+        System.out.println();
+        printOutInfoOfHelp();
+        quit:
+        while (true) {
+            String option = scanner.nextLine();
+            switch (option) {
+                case "sort":
+                    showSortByNamePrice();
+                    System.out.println();
+                    break;
+                case "top":
+                    showBestOfProdByRate();
+                    System.out.println();
+                    break;
+                case "quit":
+                    System.out.println();
+                    System.out.println("Bye Bye");
+                    break quit;
+                default:
+                    printOutInfoOfHelp();
             }
         }
+    }
 
-        private static void printOutInfoOfHelp() {
-            System.out.println(
-                    "Please choose an action. Enter :\n"
-                            + "sort - print list of products ordered according to config;\n"
-                            + "top - print top 5 products sorted via price desc;\n"
-                            + "quit - exit app;"
-            );
-        }
+    private static void printOutInfoOfHelp() {
+        System.out.println(
+                "Please choose an action. Enter :\n"
+                        + "sort - print list of products ordered according to config;\n"
+                        + "top - print top 5 products sorted via price desc;\n"
+                        + "quit - exit app;"
+        );
+    }
 
 }
 
--- store/src/main/java/com/onlineStore/coherent/RandomStorePopulator.java
+++ store/src/main/java/com/onlineStore/coherent/RandomStorePopulator.java
@@ -24,11 +24,30 @@ public class RandomStorePopulator {
         Category beer = new Beer("Beer",  productsOfBeers);
         Category medicine = new Medicine("Medicine",  productsOfMedicines);
 
-        for (int i = 0; i < 5; i++) {
-            productsOfBooks.add(new Product(faker.book().title(), faker.number().numberBetween(1, 10), faker.number().numberBetween(1, 1000)));
-            productsOfBeers.add(new Product(faker.beer().name(), faker.number().numberBetween(1, 10), faker.number().numberBetween(1, 1000)));
-            productsOfMedicines.add(new Product(faker.medical().medicineName(), faker.number().numberBetween(1, 10), faker.number().numberBetween(1, 1000)));
-        }
+//        for (int i = 0; i < 5; i++) {
+//            productsOfBooks.add(new Product(faker.book().title(), faker.number().numberBetween(1, 10), faker.number().numberBetween(1, 1000)));
+//            productsOfBeers.add(new Product(faker.beer().name(), faker.number().numberBetween(1, 10), faker.number().numberBetween(1, 1000)));
+//            productsOfMedicines.add(new Product(faker.medical().medicineName(), faker.number().numberBetween(1, 10), faker.number().numberBetween(1, 1000)));
+//        }
+
+        productsOfBooks.add(new Product("Alo", 5, 895));
+        productsOfBooks.add(new Product("Bsd", 1, 458));
+        productsOfBooks.add(new Product("Cdb", 3, 478));
+        productsOfBooks.add(new Product("Alo", 2, 789));
+        productsOfBooks.add(new Product("Frg", 9, 628));
+
+        productsOfBeers.add(new Product("Ght", 1, 854));
+        productsOfBeers.add(new Product("Kil", 8, 591));
+        productsOfBeers.add(new Product("Bnt", 10, 795));
+        productsOfBeers.add(new Product("Gho", 7, 125));
+        productsOfBeers.add(new Product("Otr", 5, 375));
+
+        productsOfMedicines.add(new Product("Hfky", 1, 632));
+        productsOfMedicines.add(new Product("Uyhn", 5, 475));
+        productsOfMedicines.add(new Product("Web", 6, 730));
+        productsOfMedicines.add(new Product("Alo", 3, 420));
+        productsOfMedicines.add(new Product("Alo", 4, 420));
+        productsOfMedicines.add(new Product("Dha", 3, 222));
 
         categories.add(book);
         categories.add(beer);
