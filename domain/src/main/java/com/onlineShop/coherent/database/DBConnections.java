bb6d245f0a94afd90db4887e68ead91d6c6eab9e
AndrewsSkBaron <baron_11_2012@mail.ru>
2022-01-21 10:27:36 +0200

    Commit init #2 http

---
+++ domain/src/main/java/com/onlineShop/coherent/database/DBConnections.java
@@ -0,0 +1,25 @@
+package com.onlineShop.coherent.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnections {
+    private static final String URL = "jdbc:mysql://localhost:3306/db_shop";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    private static Connection connection = null;
+
+    private DBConnections() {}
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
